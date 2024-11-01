9cb931a61351fc960a9572879206e60083b7e2fd
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-01 11:51:31 +0530

    04 remove from array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers,num1,num2=0,num3=0,num4=0) {
+    const n=numbers.length;
+    let i=0;
+    
+    
+        let store=[];
+        for(let i=0;i<n;i++){
+             if(numbers[i]===num1||numbers[i]===num2||numbers[i]===num3||numbers[i]===num4){
+                 continue;
+             }else{
+                store.push(numbers[i]);
+             }
+        }
+        
+        return store;
 };
 
 // Do not edit below this line
