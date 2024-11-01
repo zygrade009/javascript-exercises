9165212a0a982e12aa004b1cf6042a77f57e8460
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-01 10:02:09 +0530

    07 tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let temperature=(number-32)*5/9;
+  let temp=Math.round(temperature*10)/10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let temperature=(number*9/5)+32;
+  let temp=Math.round(temperature*10)/10;
+  return temp;
 };
 
 // Do not edit below this line
