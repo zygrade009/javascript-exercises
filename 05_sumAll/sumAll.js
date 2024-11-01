effb3e79f4dbd295bc828f09b3dceabaacee2ac7
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-01 09:47:58 +0530

    sum all done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(left ,right) {
+    let sum=0;
+    if(left<0||right<0||typeof left!=="number"||typeof right!=="number"||left%1!==0||right%1!==0){
+        return "ERROR";
+    }
+    if(left>right){
+        let temp=left;
+        left=right;
+        right=temp;
+    }
+    for(let i=left;i<=right;i++){
+        sum+=i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
