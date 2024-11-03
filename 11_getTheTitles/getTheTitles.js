64d4d709398b182b2691ffbc37a2101b88934069
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-03 16:54:16 +0530

    11 getTheTitles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+   let titles=[];
+   let n=books.length;
+   if(n===0) return titles;
+   titles=books.map((book)=>{return book.title;});
+   return titles;
 };
 
 // Do not edit below this line
