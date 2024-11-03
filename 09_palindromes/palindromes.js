8db5f9cfc1049330ce9f94d57b009dfe6860112d
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-03 16:49:28 +0530

    fibonacci done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    
+    let i=0;
+    let j=string.length-1;
+    let flag=0;
+    while(i<=j){
+       if(string[i++]!==string[j--]){
+          flag=1;
+          break;
+       }
+    }
+    if(flag===0)return true;
+    else return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+     if(number==0)return 0;
+     else if(number<0)return "OOPS";
+     else if(number==String){
+        return fibonacci(parseInt(number));
+     }
+     let numbers=[1,1];
+     let i=2;
+     while(i<number){
+        numbers.push(numbers[i-1]+numbers[i-2]);
+        i++;
+     }
+     return numbers[number-1];
 };
 
 // Do not edit below this line
